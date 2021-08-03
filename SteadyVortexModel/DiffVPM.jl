f1a3c89db34c7c8931079b47307407815575e0dd
Antonios Gementzopoulos <antoniosgeme@gmail.com>
2021-08-02 19:04:10 -0600

    Reverted back to full singularity struct

--- SteadyVortexModel/DiffVPM.jl
+++ SteadyVortexModel/DiffVPM.jl
@@ -1,9 +1,9 @@
 #= 
-This code computes the steady flow over an airfoil with
+This code computes the flow over an airfoil with
 either a sharp or blunt trailing edge using linearly varying
 line vortex panels and a linearly varying source panel if 
-needed for the trailing edge. The problem is cast as an optimization
-problem and solved using Ipopt. 
+needed for the trailing edge. Each panel consists of a line singularity
+across it as well as a collocation point in the middle
 =#
 
 include("AirfoilModule.jl")
@@ -16,7 +16,7 @@ abstract type Geometry end
 abstract type Point <: Geometry end
 abstract type Line <: Geometry end
 
-struct Panel{A,B,C,D,E,F,G,H,I,J} <: Line
+struct Panel{A,B,C,D,E,F,G,H,I} <: Line
     x_start::A
     y_start::B
     x_end::C
@@ -25,17 +25,16 @@ struct Panel{A,B,C,D,E,F,G,H,I,J} <: Line
     y_normal::F
     x_tangent::G
     y_tangent::H
-    strength_start::I
-    strength_end::J
+    singularity::I
 end 
 
-get_midpoint(p::Panel) = (x_end + x_start)/2, (y_end + y_start)/2 
+get_midpoint(p::T where T <: Line) = (p.x_end + p.x_start)/2, (p.y_end + p.y_start)/2 
 
 struct LinearStrengthLineVortex{A,B,C,D,E,F} <: LineSingularity
-    """ 
+    """
     A linear strength line vortex starting at (x_start,y_start)
     with strength γ_start and ending at (x_end,y_end) with strength
-    γ_end. It can be attached to any panel and will inherit its location properties.
+    γ_end. 
     """
     x_start::A
     y_start::B
@@ -43,31 +42,56 @@ struct LinearStrengthLineVortex{A,B,C,D,E,F} <: LineSingularity
     y_end::D
     γ_start::E 
     γ_end::F
-end  
+end 
 
 struct LinearStrengthLineSource{A,B,C,D,E,F} <: LineSingularity
     """
-    A linear strength line source starting at (x_start,y_start)
+    A linear strength line vortex starting at (x_start,y_start)
     with strength σ_start and ending at (x_end,y_end) with strength
-    σ_end. It can be attached to any panel and will inherit its location properties.
+    σ_end. 
     """
     x_start::A
     y_start::B
     x_end::C
     y_end::D
-    
     σ_start::E
     σ_end::F
 end
 
-struct CollocationPoint{T} <: Point
-    """
-    Location where no through flow is enforced on a body. 
-    It can be attached to any panel and will inherit its geometric properties.
-    """
-    x::T
-    y::T
+function induced_velocity(target::Panel,source::LinearStrengthLineVortex; unit_forcing=true)
+    dx_panel = source.x_end - source.x_start
+    dy_panel = source.y_end - source.y_start
+    panel_length = hypot(dx_panel , dy_panel)
+    xₚ_hat_x = dx_panel/panel_length
+    xₚ_hat_y = dy_panel/panel_length
+    yₚ_hat_x = - xₚ_hat_y
+    yₚ_hat_y = xₚ_hat_x
+    x_target_relative = target.x_target - source.x_start
+    y_target_relative = target.y_target - source.y_start
+    xₚ_target = x_target_relative * xₚ_hat_x + y_target_relative * xₚ_hat_y
+    yₚ_target = y_target_relative * yₚ_hat_x + y_target_relative * yₚ_hat_y
+
+    r₁ = hypot(xₚ_target, yₚ_target)
+    r₂ = hypot((xₚ_target - panel_length), yₚ_target)
+    ln_r₂_r₁ = log(r₂/r₁)
+    θ₁ = atan(yₚ_target,xₚ_target)
+    θ₂ = atan(yₚ_target,xₚ_target-panel_length)
+    dθ = θ₂ - θ₁
+
+    γ₁,γ₂ = unit_forcing ? (1,1) : (source.γ_start,source.γ_end)
+
+    uₚᵃ = (dθ*(panel_length - xₚ_target) - yₚ_target * ln_r₂_r₁) / (2π * panel_length) * γ₁
+    vₚᵃ = ((panel_length - xₚ_target) * ln_r₂_r₁ - panel_length + yₚ_target * dθ) / (2π * panel_length) * γ₁
+    uᵃ = uₚᵃ * xₚ_hat_x + vₚᵃ * yₚ_hat_x
+    vᵃ = uₚᵃ * xₚ_hat_y + vₚᵃ * yₚ_hat_y
     
+    uₚᵇ = (yₚ_target * ln_r₂_r₁  + dθ * xₚ_target) / (2π * panel_length) * γ₂
+    vₚᵇ = (xₚ_target * ln_r₂_r₁ + panel_length - yₚ_target * dθ) / (2π * panel_length) * γ₂
+    uᵇ = uₚᵇ * xₚ_hat_x + vₚᵇ * yₚ_hat_x
+    vᵇ = uₚᵇ * xₚ_hat_y + vₚᵇ * yₚ_hat_y
+
+    return uᵃ,vᵃ,uᵇ,vᵇ
 end 
+
 
 function create_panel(x_start,y_start,x_end,y_end)
